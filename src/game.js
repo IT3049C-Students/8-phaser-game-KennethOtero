359ee4d6773e96df18b76da035abe4beb4b6b780
Ken Otero <keotero99@gmail.com>
2022-10-24 15:45:17 -0400

    Added game contents

--- src/game.js
+++ src/game.js
@@ -5,10 +5,32 @@ class mainScene {
   preload() {
     // This method is called once at the beginning
     // It will load all the assets, like sprites and sounds
+
+    // Parameters: name of the sprite, path of the image
+    this.load.image('player', 'assets/player.png');
+    
+
+    this.load.image('coin', 'assets/coin.png');
   }
   create() {
     // This method is called once, just after preload()
     // It will initialize our scene, like the positions of the sprites
+
+    // Parameters: x position, y position, name of the sprite
+    this.player = this.physics.add.sprite(100, 100, 'player');
+
+    this.coin = this.physics.add.sprite(300, 300, 'coin');
+
+    // Store the score in a variable, initialized at 0
+    this.score = 0;
+
+    // The style of the text 
+    // A lot of options are available, these are the most important ones
+    let style = { font: '20px Arial', fill: '#fff' };
+
+    // Display the score in the top left corner
+    // Parameters: x position, y position, text, style
+    this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
   }
   update() {
     // This method is called 60 times per second after create()
